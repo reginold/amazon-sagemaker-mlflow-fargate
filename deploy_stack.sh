3c53fd01b809892617bf6ab1f784c700bffd1b8b
Bowen Lu <reginold.lu@gmail.com>
2022-05-14 11:41:55 +0900

    add the bootcamp but dokcer uninstall

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-npm install -g aws-cdk@2.8.0
-python3 -m venv .venv
-source .venv/bin/activate
-pip3 install -r requirements.txt
+# npm install -g aws-cdk@2.8.0
+# python3 -m venv .venv
+# source .venv/bin/activate
+# pip3 install -r requirements.txt
 
 
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account | tr -d '"')
 AWS_REGION=$(aws configure get region)
 cdk bootstrap aws://${ACCOUNT_ID}/${AWS_REGION}
 cdk deploy --parameters ProjectName=mlflow --require-approval never
+
+# https://zenn.dev/sprout2000/articles/95b125e3359694
+# before that cdk deploy --parameters ProjectName=mlflow --require-approval never , you should install the docker in wsl
